436e65f8e46576f4713b91b5175c292bd0b61f56
gastonperego <gastonperego@gmail.com>
2025-03-05 15:41:18 -0500

    fix: output

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -14,9 +14,9 @@ if [ "$file_type" != "ELF" ]; then
     exit 1
 fi
 
-magic_number=$(xxd -p -l 4 "$file_name" | tr -d '\n')
+magic_number=$(readelf -h "$file_name" | grep -oP 'Magic:\s+\K([0-9a-fA-F]{2}\s){15}[0-9a-fA-F]{2}')
 class=$(readelf -h "$file_name" | grep "Class:" | awk '{print $2}')
-byte_order=$(readelf -h "$file_name" | grep "Data:" | awk '{print $2, $3}')
+byte_order=$(readelf -h "$file_name" | grep "Data:" | awk '{print $4, $5}')
 entry_point_address=$(readelf -h "$file_name" | grep "Entry point address:" | awk '{print $4}')
 
 source messages.sh
